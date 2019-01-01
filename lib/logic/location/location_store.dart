f369d1a5b181fcecf0c7f9a2c4eada7eb437d80f
K.D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2019-01-01 23:39:44 +0530

    Fixed location lat/lang

--- lib/logic/location/location_store.dart
+++ lib/logic/location/location_store.dart
@@ -3,7 +3,7 @@ List<Map<String, dynamic>> getJsonLocations() => [
         "name": "Gorakana - Residential Facility",
         "telephone": "+94 112 685 960 / +94 114 339 606-7",
         "longitude": 79.902407,
-        "latitude": 7.8731,
+        "latitude": 6.761081,
         "fax": "+94 112 685 950",
         "email": "council@ncmh.lk",
         "address": "No. 115/2, Galkanuwa Rd, Gorakana, Panadura, Sri Lanka."
